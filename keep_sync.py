5fbbeaa0dffc0d0f85f387e757222303f6bcbf22
Brenno Almeida <contato@brennoflavio.com.br>
2024-05-11 13:53:54 -0300

    make list regex optional

--- keep_sync.py
+++ keep_sync.py
@@ -1,28 +1,28 @@
 import google_helpers
 import nextcloud_helpers
-from variables import load_and_assert
+from variables import load, load_and_assert
 
 
 def sync_shopping_list():
-    shopping_list = google_helpers.get_shopping_list(load_and_assert("NEXTCLOUD_LIST_REGEX"))
+    shopping_list = google_helpers.get_shopping_list(load("NEXTCLOUD_LIST_REGEX"))
 
     for item in shopping_list:
         nextcloud_helpers.create_task(
             item, task_list_name=load_and_assert("NEXTCLOUD_SHOPPING_LIST_NAME")
         )
 
-    google_helpers.clear_shopping_list(load_and_assert("NEXTCLOUD_LIST_REGEX"))
+    google_helpers.clear_shopping_list(load("NEXTCLOUD_LIST_REGEX"))
 
 
 def sync_notes():
-    notes = google_helpers.get_notes(load_and_assert("NEXTCLOUD_LIST_REGEX"))
+    notes = google_helpers.get_notes(load("NEXTCLOUD_LIST_REGEX"))
 
     for note in notes:
         title = note["title"]
         text = note["text"]
         nextcloud_helpers.create_note(title, text)
 
-    google_helpers.clear_notes(load_and_assert("NEXTCLOUD_LIST_REGEX"))
+    google_helpers.clear_notes(load("NEXTCLOUD_LIST_REGEX"))
 
 
 if __name__ == "__main__":
--- variables.py
+++ variables.py
@@ -8,3 +8,7 @@ def load_and_assert(variable):
     var = os.getenv(variable)
     assert var, f"Failed to retreive the following env var: {variable}"
     return var
+
+def load(variable):
+    var = os.getenv(variable)
+    return var
